bd0044cd08c97307fe50c818d2c61a77edc9af13
khamzaevasad <xamzayevasad442@gmail.com>
2025-09-01 09:03:40 +0900

    add form validation for empty search input with red border and error message

--- js/app.js
+++ js/app.js
@@ -100,28 +100,3 @@ searchBtn.addEventListener("click", async (e) => {
 
   // wordContainer.classList.remove("hidden");
 });
-
-// searchBtn.addEventListener("click", () => {
-//   let searchVal = document.getElementById("searchTerm").value;
-
-//   fetch(`${url}${searchVal}`)
-//     .then((data) => data.json())
-//     .then((data) => {
-//       const item = data[0];
-
-//       const { word, phonetic, phonetics, meanings, license, sourceUrls } = item;
-//       const audio = phonetics[0];
-
-//       //   wordContainer.innerHTML = "";
-
-//       const searchTitle = document.querySelector(".search-title");
-//       const promotion = document.querySelector(".promotion");
-//       const meaningTitle = document.querySelector(".meaning-title");
-
-//       searchTitle.textContent = word;
-//       promotion.textContent = phonetic;
-//       player.src = audio.audio;
-//       meaningTitle.textContent = meanings[0].partOfSpeech;
-//       console.log(meaningTitle);
-//     });
-// });
